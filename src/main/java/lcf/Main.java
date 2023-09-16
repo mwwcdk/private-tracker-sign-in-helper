f170dc928bed73d4d59d9826cc14c1805ab750a6
cdk <lcfcdk@163.com>
2023-09-16 18:51:59 +0800

    日志

--- src/main/java/lcf/Main.java
+++ src/main/java/lcf/Main.java
@@ -18,17 +18,25 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        log.info("程序开始启动...");
-        long startTime = System.currentTimeMillis();
-        // 这里使用了Netty提供的事件循环组, 用于做定时调度
-        EventLoopGroup eventLoopGroup = new DefaultEventLoopGroup();
-        eventLoopGroup.scheduleAtFixedRate(() -> {
-            log.info("定时调度, 当前秒:" + System.currentTimeMillis() / 1000);
-            System.out.println("定时调度, 当前秒:" + System.currentTimeMillis() / 1000);
-        }, 5000, 2000, TimeUnit.MILLISECONDS);
-        // 添加程序关闭钩子
-        Runtime.getRuntime().addShutdownHook(new ShutdownHook(eventLoopGroup));
-        log.info("恭喜 程序启动成功! 耗时:{}ms", System.currentTimeMillis() - startTime);
+        try {
+            log.info("程序开始启动...");
+            long startTime = System.currentTimeMillis();
+            // 这里使用了Netty提供的事件循环组, 用于做定时调度
+            EventLoopGroup eventLoopGroup = new DefaultEventLoopGroup();
+            eventLoopGroup.scheduleAtFixedRate(() -> {
+                try {
+                    sign();
+                } catch (Exception e) {
+                    log.error("定时任务执行异常", e);
+                }
+            }, 5000, 2000, TimeUnit.MILLISECONDS);
+            // 添加程序关闭钩子
+            Runtime.getRuntime().addShutdownHook(new ShutdownHook(eventLoopGroup));
+            log.info("恭喜 程序启动成功! 耗时:{}ms", System.currentTimeMillis() - startTime);
+        } catch (Throwable e) {
+            log.error("程序启动失败o(╥﹏╥)o", e);
+            Runtime.getRuntime().exit(1);
+        }
     }
 
 }
